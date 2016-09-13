15361790d80c7b9d5515a85e3fa85ccf30a6edd5
Hunter Damron <hdamron1594@yahoo.com>
2016-09-13 16:33:20 -0400

    Added back everything not ignored (excludes build, dist, docs, nbproject)

---
+++ gradebook.cpp
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   gradebook.cpp
+ * Author: hdamron1594
+ * 
+ * Created on September 9, 2016, 10:18 PM
+ */
+
+#include "gradebook.h"
+
+string gradebook::HELP = 
+    "Gradebook, version 1.0\n"
+    "Usage:\n"
+    "  help                                     \tDisplays this message\n"
+    "  select                                   \tSelects defaults\n"
+    "     section - <section name>              \t  Selects section\n"
+    "     student [options] - <student name>    \t  Selects student\n"
+    "        -sect <section name>               \t    Specifies section\n"
+    "  delete                                   \tDeletes an attribute\n"
+    "     section [options] - <section name>    \t  Deletes section\n"
+    "        -a                                 \t    deletes all sections (replace \'- <section name>\')\n"
+    "     student [options] - <student name>    \t  Deletes student\n"
+    "        -sect <section name>               \t    Overrides student selection\n"
+    "        -a                                 \t    deletes all students in selected section\n"
+    "     grade [options] - <grade key>         \t  Deletes grade from student\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "        -stu <student name>                \t    Overrides student selection\n"
+    "        -a                                 \t    Deletes all grades in selected section and student\n"
+    "  add                                      \tAdds an attribute\n"
+    "     section [options] - <section name>    \t  Adds section\n"
+    "     student [options] - <student name>    \t  Adds student to section\n"
+    "	 -sect <section name>               \t    Overrides section selection\n"
+    "     grade [options] - <grade key>         \t  Adds grade to student\n"
+    "        -sect <section name>               \t     Overrides section selection\n"
+    "        -stu <student name>                \t     Overrides student selection\n"
+    "  change                                   \tChanges an attribute\n"
+    "     grade [options] - <grade key>         \t  Changes grade for student\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "        -stu <student name>                \t    Overrides student selection\n"
+    "  lookup                                   \tSearches by keyword\n"
+    "     section [options] - <keyword>         \t  Searches sections\n"
+    "     student [options] - <keyword>         \t  Searches students\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "     grade [options] - <grade key>         \t  Searches grades\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "        -stu <student name>                \t    Overrides student selection\n"
+    "  display                                  \tDisplays gradebook\n"
+    "     portfolio [options] [- <student name>]\t  displays all grades for single student\n"
+    "        -sect <section name>               \t     Overrides section selection\n"
+    "     sections                              \t  lists sections\n"
+    "     students                              \t  lists students in selected section\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "     grades [options] - <gradekey>         \t  displays single grade for all students\n"
+    "        -sect <section name>               \t    Overrides section selection\n"
+    "        -stu <student name>                \t    Overrides student selection\n"
+    "     section [- <section name>]            \t  displays everything about current section\n"
+    "        -a                                 \t    Displays all sections at once\n"
+    "  quit                                     \tQuits the program\n"
+    "* Note that -sect and -stu flags temporarily override defaults\n"
+    "     If not included, selected defaults are used";
+
+string gradebook::WELCOME = 
+    "Welcome to Gradebook, version 1.0\n"
+    "This program is designed to help teachers help students.\n"
+    "Type help for usage instructions";
+
+string gradebook::PROMPT = "GRADEBOOK> ";
+
+/**
+ * Default constructor
+ */
+gradebook::gradebook() {
+    select_book = "";
+    select_student = "";
+    terminate = false;
+    
+    
+}
+
+gradebook::~gradebook() {
+}
+
+void gradebook::parse(string line) {
+    vector<string> tokens = tokenize(line);
+    if(tokens[0] == "help") {
+        
+    } else if(tokens[0] == "select") {
+        if(tokens[1] == "section") {
+            
+        } else if(tokens[1] == "student") {
+            
+        } 
+    } else if(tokens[0] == "delete") {
+        
+    } else if(tokens[0] == "add") {
+        
+    } else if(tokens[0] == "change") {
+        
+    } else if(tokens[0] == "lookup") {
+        
+    } else if(tokens[0] == "display") {
+        
+    } else if(tokens[0] == "quit") {
+        terminate = true;
+    } else {
+        
+    }
+}
+
+bool gradebook::term() const {
+    return terminate;
+}
+
+vector<string> gradebook::tokenize(string str) {
+    return tokenize(str, " ");
+}
+
+vector<string> gradebook::tokenize(string str, string delimiter) {
+    vector<string> tokens;
+    int start = 0;
+    int end = 0;
+    while(end != string::npos) {
+        end = str.find(delimiter, start);
+        string sub = str.substr(start, end - start);
+        if(sub.size() > 0) {
+            tokens.push_back(sub);
+        }
+        start = end + delimiter.size();
+    }
+    return tokens;
+}
+
+string gradebook::detokenize(vector<string> tokens, int begin) {
+    if(begin > tokens.size() - 1) {
+        throw out_of_range("Beginning index is not in vector");
+    }
+    //TODO finish putting string vector back together
+}
+
---
+++ gradebook.h
@@ -0,0 +1,110 @@
+/* 
+ * File:   gradebook.h
+ * Author: hdamron1594
+ *
+ * Created on September 5, 2016, 10:30 AM
+ */
+
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class student {
+public:
+    //constructors and destructors
+    explicit student(string, string); //Constructor with first and last name
+    virtual ~student(); //Default destructor
+    
+    //accessors
+    string get_name() const; //Gets full student name
+    string get_first_name() const; //Gets first name
+    string get_last_name() const; //Gets last name
+    double average() const; //Gets average of all grades
+    map<string, double> get_grades() const; //Gets map of all grades
+    double get_grade(string) const; //Gets grade under specified key
+    
+    //mutators
+    void change_grade(string, double); //Changes grade under specified key
+    void delete_grade(string); //Deletes grade at specified key
+    
+private:
+    string first_name;
+    string last_name;
+    map<string, double> grades;
+
+};
+
+#endif /* STUDENT_H */
+
+
+#ifndef ROSTER_H
+#define ROSTER_H
+
+class section {
+public:
+    //constructors and destructors
+    explicit section(string); //Constructor with class name
+    virtual ~section(); //Default constructor
+    
+    //accessors
+    vector<string> lookup(string) const; //Finds student names
+    map<string, double> display(string) const; //Gets grades map
+    double get_grade(string, string) const; //Gets grade with specified key
+    double average(string) const; //Gets average of all grades
+    map<string, double> display_set(string) const;//Gets specified grade for all
+    map<string, double> all_averages() const; //Gets averages for all students
+    map<string, map<string, double> > display_all() const; //Gets everything
+    
+    //mutators
+    void expell(string); //Deletes student at specified key
+    void change_grade(string, string, double);//Changes grade with specified key
+    void delete_grade(string, string); //Deletes grade with specified key
+    void add_student(string, string); //Adds student to roster
+    void add_all(vector<array<string,2> >); //Adds multiple students
+    
+private:
+    map<string, student> students;
+    string classname;
+
+};
+
+#endif /* ROSTER_H */
+
+#ifndef GRADEBOOK_H
+#define GRADEBOOK_H
+
+class gradebook {
+public:
+    //constructors and destructors
+    explicit gradebook();
+    virtual ~gradebook();
+    
+    //accessors
+    bool term() const; //returns true if it is ready to terminate looping
+    
+    //mutators
+    void parse(string); //parses and executes string command
+    
+    //static methods and variables
+    static vector<string> tokenize(string); //tokenizes string; delimiter=" "
+    static vector<string> tokenize(string, string); //tokenizes string
+    static string detokenize(vector<string>, int); //puts vector back together
+    
+    static string HELP;
+    static string WELCOME;
+    static string PROMPT;
+    
+private:
+    map<string, section> books;
+    string select_book;
+    string select_student;
+    bool terminate;
+};
+
+#endif /* GRADEBOOK_H */
+
